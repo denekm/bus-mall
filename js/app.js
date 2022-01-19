ad7bb55cbdabcc6be965c8ef47c189288652d784
Marta <mrtdeneke@gmail.com>
2022-01-19 15:52:33 -0800

    fixed js

--- js/app.js
+++ js/app.js
@@ -7,41 +7,39 @@ let counterMaxValue = 25;
 const myContainer = document.querySelector('section');
 const myButton = document.querySelector('section + div');
 
-let img1 = document.querySelector('select img:first-child');
-let img2 = document.querySelector('select img:nth-child(2)');
-let img3 = document.querySelector('select img:nth-child(3)');
+let image1 = document.querySelector('section img:first-child');
+let image2 = document.querySelector('section img:nth-child(2)');
+let image3 = document.querySelector('section img:nth-child(3)');
 
 function Product(name, fileExtension = 'jpeg') {
-  this.name = name;
   this.likes = 0;
   this.views = 0;
-  this.src = `images/${name}.${fileExtension}`;
+  this.name = name;
+  this.src =`img/${name}.${fileExtension}`;
   imgArray.push(this);
 }
 
-function selectProduct() {
+function selectRandomImg() {
   return Math.floor(Math.random() * imgArray.length);
 }
 
 function renderImages() {
-  let img1 = selectRandomImg();
-  let img2 = selectRandomImg();
-  let img3 = selectRandomImg();
-  while(img1 === img2) {
-    img2 = selectRandomImg();
+  let product1 = selectRandomImg();
+  let product2 = selectRandomImg();
+  let product3 = selectRandomImg();
+  while(product1 === product2) {
+    product2 = selectRandomImg();
   }
-
-  img1.src = imgArray[img1].src;
-  img1.alt = imgArray[img1].name;
-  img2.src = imgArray[img2].src;
-  img2.alt = imgArray[img2].name;
-  img3.src = imgArray[img3].src;
-  img3.alt = imgArray[img3].name;
-
-  imgArray[img1].views++;
-  imgArray[img2].views++;
-  imgArray[img3].views++;
-
+  console.log(image1);
+  image1.src = imgArray[product1].src;
+  image1.alt = imgArray[product1].name;
+  image2.src = imgArray[product2].src;
+  image2.alt = imgArray[product2].name;
+  image3.src = imgArray[product3].src;
+  image3.alt = imgArray[product3].name;
+  imgArray[product1].views++;
+  imgArray[product2].views++;
+  imgArray[product3].views++;
 }
 
 function handleClick(event) {
@@ -56,9 +54,47 @@ function handleClick(event) {
       break;
     }
   }
-  new Product('bag');
-  new Product('banana');
-  new Product('bathroom');
-
+  if (counter === counterMaxValue) {
+    myContainer.removeEventListener('click', handleClick);
+    myButton.className = 'clicks-allowed';
+    myButton.addEventListener('click', handleButtonClick);
+  }
   renderImages();
 }
+function handleButtonClick() {
+  if(counter === counterMaxValue) {
+    renderResults();
+  }
+}
+function renderResults() {
+  let ul = document.querySelector('ul');
+  for (let i =0; i< imgArray.length; i++) {
+    let message = `${imgArray[i].name} had ${imgArray[i].views} views & it was clicked on ${imgArray[i].likes} times`;
+    let li = document.createElement('li');
+    li.textContent = message;
+    ul.appendChild(li);
+  }
+}
+new Product('bag');
+new Product('banana');
+new Product('bathroom');
+new Product('boots');
+new Product('bubblegum');
+new Product('chair');
+new Product('cthulhu');
+new Product('dog-duck');
+new Product('dragon');
+new Product('pen');
+new Product('pet-sweep');
+new Product('scissors');
+new Product('shark');
+new Product('sweep', 'png');
+new Product('tauntaun');
+new Product('unicorn');
+new Product('water-can');
+new Product('wine-glass');
+
+
+renderImages();
+
+myContainer.addEventListener('click', handleClick);
